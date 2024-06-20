7505b499a4fc0b0d2e7ea84b1f359e7d8a2155e6
신재건 <guneleven9911@gmail.com>
2024-06-20 22:24:16 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect } from 'react';
+import Movie from './components/Movie';
+import Deatil from './routes/Deatil';
+import Home from './routes/Home';
+import {
+  BrowserRouter as Router, Route, Routes, Link
+} from "react-router-dom";
+function App(props) {
+  return <Router>
+    <Routes>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}/> 
+      <Route path='/movie/:id' element={<Deatil/>}/>
+    </Routes>
+  </Router>
 }
 
 export default App;
---
+++ src/components/Movie.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import {
+    BrowserRouter as Router, Route, Routes, Link
+  } from "react-router-dom";
+function Movie({id,coverImg,title,summary,genres}) {
+    return (
+        <div >
+        <img src={coverImg}></img>
+        <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
+        <p>{summary}</p>
+        <ul>{genres.map((g) => 
+        <li key={g}>{g}</li>)}</ul>
+      </div>
+    );
+}
+
+Movie.propTypes={
+    id:PropTypes.number.isRequired,
+    coverImg:PropTypes.string.isRequired,
+    title:PropTypes.string.isRequired,
+    summary:PropTypes.string.isRequired,
+    genres:PropTypes.arrayOf(PropTypes.string).isRequired
+}
+export default Movie;
